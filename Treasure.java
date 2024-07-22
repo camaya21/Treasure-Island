c6c7b4de5d2c1da714018b6a98986cb119f97060
camaya21 <cindymamaya21@gmail.com>
2024-07-22 11:37:26 -0400

    2D array implemented

--- Treasure.java
+++ Treasure.java
@@ -1,9 +1,40 @@
 /**
  * Treasure
  */
+import java.util.Scanner;
 public class Treasure {
 
     public static void main(String[] args) {
-        System.out.println("yooooo");
+        boolean[][] treasure = new boolean[7][7];
+        boolean[][] tree = new boolean[7][7];
+        boolean[][] mound = new boolean [7][7];
+        boolean[][] board = new boolean [7][7];
+        boolean gameOver = false;
+        int row = 0;
+        int column = 0;
+        int userPoints = 0;
+
+        Scanner myObj = new Scanner(System.in);
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++){
+                treasure[i][j] = false;
+            }
+        }
+
+        treasure[5][4] = true;
+        tree[1][2] = true;
+        tree[4][1] = true;
+        mound[2][0] = true;
+        mound[3][4] = true;
+        
+        for (int i = 0; i < 7; i++){
+            System.out.println();
+            for (int j = 0; j< 7; j++){
+                System.out.print(" ? ");
+            }
+        }
+
+        
     }
 }
